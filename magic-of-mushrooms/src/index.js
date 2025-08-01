8dbdd17fc1d3a53d955af99bae88c1a2ede2ec2c
holyfudgencannoli <moshimoshi8246@gmail.com>
2025-08-01 18:24:43 -0400

    Update index.js

--- magic-of-mushrooms/src/index.js
+++ magic-of-mushrooms/src/index.js
@@ -4,7 +4,7 @@ export default {
       return new Response(null, {
         status: 204,
         headers: {
-          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Origin": "https://magic-of-mushrooms-frontend.moshimoshi8246.workers.dev/",
           "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
           "Access-Control-Allow-Headers": "Content-Type",
         },
@@ -14,8 +14,9 @@ export default {
     return new Response(JSON.stringify({ msg: "Hello from Worker!" }), {
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Origin": "https://magic-of-mushrooms-frontend.moshimoshi8246.workers.dev/",
       },
     });
   },
 };
+

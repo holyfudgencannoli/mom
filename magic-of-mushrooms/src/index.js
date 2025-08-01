9e16aecc611cd7ce4a053ebca7a42af24f7abfc4
holyfudgencannoli <moshimoshi8246@gmail.com>
2025-08-01 18:07:50 -0400

    Update index.js

--- magic-of-mushrooms/src/index.js
+++ magic-of-mushrooms/src/index.js
@@ -1,7 +1,21 @@
 export default {
   async fetch(request) {
-    return new Response("Hello from Worker!", {
-      headers: { "content-type": "text/plain" }
+    if (request.method === "OPTIONS") {
+      return new Response(null, {
+        status: 204,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type",
+        },
+      });
+    }
+
+    return new Response(JSON.stringify({ msg: "Hello from Worker!" }), {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
     });
-  }
+  },
 };
